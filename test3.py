208484d22f6b06b318bd7d48cdbd3d0b07d71fcc
jonptest <jonpmacdonald@gmail.com>
2026-04-13 16:49:06 -0400

    Add files via upload

--- test3.py
+++ test3.py
@@ -24,11 +24,11 @@ input_data = pd.DataFrame([[pclass, age, sibsp, parch, sex]],
 # Predict Button
 if st.button("Predict Survival"):
     prediction = model.predict(input_data)[0]
-    probs = model.predict_proba(input_data)[0]
-    survival_prob = probs[1]
+    probs = model.predict_proba(input_data)[0, 1]
+  
     if prediction == 1:
         st.success(f"Result: **Survived**")
     else:
         st.error(f"Result: **Did Not Survive**")
     
-    st.metric(label="Survival Probability", value=f"{probability:.2%}")
+    st.metric(label="Survival Probability", value=f"{probs:.2%}")
